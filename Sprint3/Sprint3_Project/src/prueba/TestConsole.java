3f0b8d3472f824e02547bee00aee2d9806071338
carlosgian <carmijor@uni.pe>
2023-04-26 10:11:21 -0500

    TestConsole in Sprint2

--- Sprint3/Sprint3_Project/src/prueba/TestConsole.java
+++ Sprint3/Sprint3_Project/src/prueba/TestConsole.java
@@ -1,4 +1,28 @@
 package prueba;
 
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import produccion.SOSGameBoard;
+import produccion.Console;
+
+import java.util.Random;
+
 public class TestConsole {
+
+    static Random random = new Random();
+    static int rand = random.nextInt(8) + 3;
+    public static int sideLength = rand;
+
+    public static SOSGameBoard board;
+
+    @BeforeAll
+    public static void setUp() {
+        board = new SOSGameBoard(sideLength);
+    }
+
+    @Test
+    public void testEmptyBoardOnConsole(){
+        new Console(board).displayBoard();
+    }
 }
